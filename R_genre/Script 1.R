c78770d79a7004cd7c81ad8969d564fc32107812
Élodie Lemaire <72803949+ElodieXVI@users.noreply.github.com>
2021-11-25 16:40:50 +0100

    Verfasser gender

--- R_genre/Script 1.R
+++ R_genre/Script 1.R
@@ -7,7 +7,7 @@ library(tidyr)
 library(questionr)
 
 
-data <- read_excel("Constance_de_Salm_Special_lettres copie.xlsx")
+data <- read_excel("Constance_de_Salm_Special_lettres.xlsx")
 data$ID <- data$...1
 data$pers_autre <- data$`Weitere Personen`
 
@@ -51,7 +51,7 @@ prenom$gender
 data_genre <- left_join(data3, select(prenom, name, gender), by= c("prenom1"= "name"))
 
 
-prenom <- gender(data3$prenom2, method = c("ssa", "ipums", "napp", "kantrowitz", "genderize", "demo"), countries = c("United States", "Canada", "United Kingdom", "France", "Italia", "Germany"), years = c(1950, 2012))
+prenom <- gender(data3$prenom2, method = c("ssa", "ipums", "napp", "kantrowitz", "genderize", "demo"), countries = c("United States", "Canada", "United Kingdom", "France", "Italia", "Germany"), years = c(1807, 1842))
 
 prenom$name
 prenom$gender
@@ -59,28 +59,15 @@ prenom$gender
 data_genre_comp <- left_join(data_genre, select(prenom, name, gender), by= c("prenom2"= "name"))
 
 
-library(ggplot2)
-
-ggplot(data_genre_comp) +
- aes(x = Ausstellungsort) +
- geom_bar(fill = "#112446") +
- theme_minimal()
-
-ggplot(data_genre_comp) +
- aes(x = `Datierung (JJJJ-MM-TT)`) +
- geom_bar(fill = "#112446") +
- theme_minimal()
-
-library(lubridate)
+data_genre_comp$Weitere_Pers1_genre <- data_genre_comp$gender.x
+data_genre_comp$Weitere_Pers2_genre <- data_genre_comp$gender.y
+data_genre_comp$gender.y <- NULL
+data_genre_comp$gender.x <- NULL
 
 ###Quelques statistiques sur les genres des personnes mentionnées dans ses lettres : 
 
-freq(data_genre_comp$gender.x)
-freq(data_genre_comp$gender.y)
-
-
-
-library(ggplot2)
+freq(data_genre_comp$Weitere_Pers1_genre)
+freq(data_genre_comp$Weitere_Pers2_genre)
 
 ggplot(data_genre_comp) +
  aes(x = `Datierung (JJJJ-MM-TT)`, fill = gender.y) +
@@ -94,28 +81,78 @@ ggplot(data_genre_comp) +
  scale_fill_hue(direction = 1) +
  theme_minimal()
 
-
-library(lubridate)
-
 ####Genre des auteurs : ####
-data <- data_genre_comp
-table(data$Verfasser)
-data <- data  %>% 
-  separate(Verfasser, sep=",", into=c("Verfasser_nom", "Verfasser_prénom"))
-data$Verfasser_prénom
+df <- read_excel("Constance_de_Salm_Special_lettres.xlsx")
+df$ID <- data$...1
 
-data_2 <- data %>% select("ID","Verfasser_nom", "Verfasser_prénom")
+## Recodage de df$Verfasser en df$Verfasser_rec
+df$Verfasser_rec <- fct_recode(df$Verfasser,
+  "Monsieur" = "Chambert, Monsieur (Anwalt CdS' in Tours)",
+  "Pierre" = "Drais, Pierre (Vertrauensmann der CdS)",
+  "Pierre ; Constance" = "Drais, Pierre (Vertrauensmann der CdS); Salm, Constance de (CdS)",
+  "Alexandre" = "Duval, Alexandre, eigentl. Alexandre-Vincent Pineux",
+  "Amaury" = "Duval, Amaury, eigentl. Charles-Alexandre-Amaury Pineux",
+  "Louis-Mathieu" = "Langlès, Louis-Mathieu",
+  "Edme" = "Mentelle, Edme",
+  "Jean Baptiste Joseph" = "Pailliet, Jean Baptiste Joseph (Anwalt der CdS in Orléans, um 1829)",
+  "Constance" = "Salm, Constance de (CdS)",
+  "Adolphe" = "Théis, Adolphe de",
+  "Alexandre" = "Théis, Alexandre Étienne Guillaume, baron de (Bruder der CdS)",
+  "Alexandre ; Adolphe" = "Théis, Alexandre Étienne Guillaume, baron de (Bruder der CdS); Théis, Adolphe de",
+  "Alexandre ; Anne Marguerite" = "Théis, Alexandre Étienne Guillaume, baron de (Bruder der CdS); Théis, Anne Marguerite de, geb. de Saint-Mars (Frau des Bruders der CdS)",
+  "Anne Marguerite" = "Théis, Anne Marguerite de, geb. de Saint-Mars (Frau des Bruders der CdS)",
+  "Anne Marguerite ; Joséphine" = "Théis, Anne Marguerite de, geb. de Saint-Mars (Frau des Bruders der CdS); Théis, Joséphine de, verh. Saint-Cricq, gen. Finette",
+  "Charles" = "Théis, Charles Constant de",
+  "Joséphine" = "Théis, Joséphine de, verh. Saint-Cricq, gen. Finette",
+  "Jean-François" = "Thurot, Jean-François")
+df$Verfasser_rec <- as.character(df$Verfasser_rec)
 
-data_2 <- data_2 %>% mutate_if(is.character, funs(str_replace_all(.," ", "_")))
+df <- df  %>% 
+  separate(Verfasser_rec, sep=";", into=c("Verfasser_1", "Verfasser_2"))
 
-data_2 <- data_2  %>% 
-  separate(Verfasser_prénom, sep="_", into=c("null", "Verfasseur_prenom1"))
-data_2$null <- NULL
+df_2 <- df %>% select("ID","Verfasser_1", "Verfasser_2")
 
-
-prenom <- gender(data_2$Verfasseur_prenom1, method = c("ssa", "ipums", "napp", "kantrowitz", "genderize", "demo"), countries = c("United States", "Canada", "United Kingdom", "France", "Italia", "Germany"), years = c(1950, 2012))
+prenom1 <- gender(df_2$Verfasser_1, method = c("ssa", "ipums", "napp", "kantrowitz", "genderize", "demo"), countries = c("United States", "Canada", "United Kingdom", "France", "Italia", "Germany"), years = c(1807, 1842))
 prenom$gender
+prenom$name
 
-data_2 <- left_join(data_2, select(prenom, name, gender), by= c("Verfasseur_prenom1"= "name"))
+df_3 <- left_join(df, select(prenom, name, gender), by= c("Verfasser_1"= "name"))
+
+table(df_3$gender, df_3$Verfasser_1, useNA = "ifany")
+
+df_3$gender [df_3$Verfasser_1 == "Alexandre"] <- "Male"
+df_3$gender [df_3$Verfasser_1 == "Monsieur"] <- "Male"
+df_3$gender [df_3$Verfasser_1 == "Constance"] <- "Female"
+df_3$gender [df_3$Verfasser_1 == "Anne Marguerite"] <- "Female"
+df_3$gender [df_3$Verfasser_1 == "Edme"] <- "Male"
+df_3$gender [df_3$Verfasser_1 == "Anne Marguerite "] <- "Female"
+df_3$gender [df_3$Verfasser_1 == "Jean Baptiste Joseph"] <- "Male"
+df_3$gender [df_3$Verfasser_1 == "Pierre "] <- "Male"
+df_3$gender [df_3$Verfasser_1 == "Louis-Mathieu"] <- "Male"
+df_3$gender [df_3$Verfasser_1 == "Joséphine"] <- "Female"
+df_3$gender [df_3$Verfasser_1 == "Jean-François"] <- "Male"
+df_3$gender [df_3$Verfasser_1 == "Alexandre "] <- "Male"
+
+## Recodage de df_3$gender en df_3$Verfasser1_gender
+df_3$Verfasser1_gender <- fct_recode(df_3$gender,
+  "Male" = "male")
+
+df_3$gender <- NULL
+
+freq(df_3$Verfasser1_gender)
+
+#Pour les deuxième auteur : ####
+
+## Recodage de df_3$Verfasser_2 en df_3$Verfasser2_gender
+df_3$Verfasser2_gender <- fct_recode(df_3$Verfasser_2,
+  "Male" = " Adolphe",
+  "Female" = " Anne Marguerite",
+  "Female" = " Constance",
+  "Female" = " Joséphine")
 
 
+#Écriture du fichier :
+df_3$Verfasser_1 <- NULL
+df_3$Verfasser_2 <- NULL
+
+write_csv2(df_3, "CdS_Lettres_genre.csv")
